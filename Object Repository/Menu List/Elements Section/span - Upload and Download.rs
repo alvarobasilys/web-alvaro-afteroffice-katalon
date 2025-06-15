5808af5be5947e80e9728bd81168582c3658aac6
Alvaro Basily <Alvaro Basily@alvarobasilys>
2025-06-15 18:58:56 +0700

    New TC "Verify Download - Upload and Download Page"

---
+++ Object Repository/Menu List/Elements Section/span - Upload and Download.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span - Upload and Download</name>
+   <tag></tag>
+   <elementGuidId>7d48eef3-f89b-43ff-841b-b06adb5d9155</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[text() = &quot;Upload and Download&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
