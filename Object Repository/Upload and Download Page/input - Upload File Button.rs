2e1bb9a646e8de49f9e6151a720f38aaecc4f23c
Alvaro Basily <Alvaro Basily@alvarobasilys>
2025-06-15 23:15:52 +0700

    Add new TC, Verify Upload - Upload and Download Page

---
+++ Object Repository/Upload and Download Page/input - Upload File Button.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input - Upload File Button</name>
+   <tag></tag>
+   <elementGuidId>52d2ea3e-592b-4dab-8dad-2985bf5b024c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id = &quot;uploadFile&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Upload and Download Page/p - Upload Output Text.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>p - Upload Output Text</name>
+   <tag></tag>
+   <elementGuidId>ed6ef118-f9ba-45a3-b5c8-4481460f12e0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//p[@id = &quot;uploadedFilePath&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
